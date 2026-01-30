ac529730ab817832bad8380f1148a70e49305425
SapphireFeiFei24 <sunxiaofei@sunxiaofeis-MacBook-Air.local>
2026-01-30 21:56:17 +0800

    delete directly with the raw data

--- backend/files/models.py
+++ backend/files/models.py
@@ -1,12 +1,16 @@
-from django.db import models
-import uuid
 import os
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import uuid
+
 
 def file_upload_path(instance, filename):
     """Generate file path for new file upload"""
     ext = filename.split('.')[-1]
     filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join('uploads', filename)
+
 
 class File(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -15,9 +19,17 @@ class File(models.Model):
     file_type = models.CharField(max_length=100)
     size = models.BigIntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-uploaded_at']
-    
+
     def __str__(self):
         return self.original_filename
+
+
+# Signal to delete the file from filesystem when the model instance is deleted
+@receiver(post_delete, sender=File)
+def delete_file_from_storage(sender, instance, **kwargs):
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
